7ff9e4ad171330376b3a26cddf6276f9f8b029ef
interdigitize <interdigitize@users.noreply.github.com>
2017-06-18 19:18:19 -0700

    Adds a fathers day card.

---
+++ digital-fathers-day-card/js/main.js
@@ -0,0 +1,10 @@
+// $('#app')
+
+$('#text').click('click', removeEnvelope);
+
+function removeEnvelope(){
+  $('body').empty();
+  $('body').css('background', 'url("imgs/grid.jpg")');
+  $('body').prepend('<div id="a-note"><p>Dear Dad,</p><p>I know you love a good solvable puzzle, so I got you one for Father\'s Day! &#x1F60A Have a lovely day!</p><p>Love,<br>Me</p></div>');
+  $('body').append('<div id="sudoku"><iframe src="http://widget.websudoku.com/?level=3" width="200" height="260" scrolling="no" frameborder="0"></iframe></div>')
+}
